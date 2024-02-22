f2ff91f4e38ab8a85667c1ec8454382ecd556086
Василий <greybasil2000@yandex.ru>
2024-02-22 14:46:01 +0300

    Modify MainMenuScene
    Add main menu animations, sounds, font, sound script.

---
+++ Assets/Scripts/ChooseLevelBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseLevelBehaviour : MonoBehaviour
+{
+    [SerializeField] private AudioSource asseptSound;
+    [SerializeField] private AudioSource declineSound;
+
+    private string maxLevelKey = "MaxLevel";
+    private string musicVolumeKey = "MusicVolumeKey";
+
+    /// <summary>
+    /// Check level availability by level number.
+    /// </summary>
+    /// <param name="lvl">Level number.</param>
+    public void CheckLevelAvailability(int lvl)
+    {
+        if (!PlayerPrefs.HasKey(musicVolumeKey))
+        {
+            PlayerPrefs.SetFloat(musicVolumeKey, 0.2f);
+        }
+
+        if (!PlayerPrefs.HasKey(maxLevelKey))
+        {
+            PlayerPrefs.SetInt(maxLevelKey, 1);
+        }
+        else if (lvl <= PlayerPrefs.GetInt(maxLevelKey, 1000))
+        {
+            if (PlayerPrefs.GetFloat(musicVolumeKey, 0.2f) != 0f)
+            {
+                asseptSound.volume = PlayerPrefs.GetFloat(musicVolumeKey, 0.2f) + 0.2f;
+                asseptSound.Play();
+            }
+        }
+        else
+        {
+            if (PlayerPrefs.GetFloat(musicVolumeKey, 0.2f) != 0f)
+            {
+                declineSound.volume = PlayerPrefs.GetFloat(musicVolumeKey, 0.2f) + 0.2f;
+                declineSound.Play();
+            }
+        }
+    }
+}
